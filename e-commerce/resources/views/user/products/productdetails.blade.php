2e1d2e4dac67821401b176fd8d22c9dc5a53c088
MohamedAhmedHassan894 <mohamed.bebo89@gmail.com>
2021-01-23 05:10:19 +0200

    review function added

--- e-commerce/resources/views/user/products/productdetails.blade.php
+++ e-commerce/resources/views/user/products/productdetails.blade.php
@@ -11,7 +11,8 @@
     <!-- owl-carousel -->
     <link rel="stylesheet" type="text/css" href="{{ url('/css/owl.carousel.min.css') }}">
     <!-- font awesome link -->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+        integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ url('/css/font-awesome.min.css') }}">
     <!-- animate css -->
     <!-- jquery ui -->
@@ -27,11 +28,10 @@
     <link rel="stylesheet" type="text/css" href="{{ url('/css/style-starter.css') }}">
     <!-- <link rel="icon" type="..imgs/favicon.png" href="../imgs/favicon.png"> -->
     <style>
-        .btnss:hover
-        {
-            background-color:white ;
-            color:black;
-        }
+    .btnss:hover {
+        background-color: white;
+        color: black;
+    }
     </style>
 </head>
 
@@ -39,43 +39,43 @@
     <!-- header -->
     @include("user.layouts.firstnav")
     <div class="header-inner">
-                <div class="container">
-                    <div class="cat-nav-head">
-                        <div class="row">
-                            <div class="col-lg-3">
+        <div class="container">
+            <div class="cat-nav-head">
+                <div class="row">
+                    <div class="col-lg-3">
 
-                            </div>
-                            <div class="col-lg-9 col-12">
-                                <div class="menu-area">
-                                    <!-- Main Menu -->
-                                    <nav class="navbar navbar-expand-lg">
-                                        <div class="navbar-collapse">
-                                            <div class="nav-inner">
-                                                <ul class="nav main-menu menu navbar-nav">
-                                                    <li class="active"><a href="#">Home</a></li>
-                                                    <li><a href="/allcategories">Products</a></li>
-                                                    <li><a >Shop<i class="ti-angle-down"></i><span class="new">New</span></a>
-                                                        <ul class="dropdown">
-                                                            <li><a href="/cartdetails">Cart</a></li>
-                                                            <li><a href="/checkout">Checkout</a></li>
-                                                        </ul>
-                                                    </li>
-
-
-                                                    <li><a href="/contactus">Contact Us</a></li>
+                    </div>
+                    <div class="col-lg-9 col-12">
+                        <div class="menu-area">
+                            <!-- Main Menu -->
+                            <nav class="navbar navbar-expand-lg">
+                                <div class="navbar-collapse">
+                                    <div class="nav-inner">
+                                        <ul class="nav main-menu menu navbar-nav">
+                                            <li class="active"><a href="#">Home</a></li>
+                                            <li><a href="/allcategories">Products</a></li>
+                                            <li><a>Shop<i class="ti-angle-down"></i><span class="new">New</span></a>
+                                                <ul class="dropdown">
+                                                    <li><a href="/cartdetails">Cart</a></li>
+                                                    <li><a href="/checkout">Checkout</a></li>
                                                 </ul>
-                                            </div>
-                                        </div>
-                                    </nav>
-                                    <!--/ End Main Menu -->
+                                            </li>
+
+
+                                            <li><a href="/contactus">Contact Us</a></li>
+                                        </ul>
+                                    </div>
                                 </div>
-                            </div>
+                            </nav>
+                            <!--/ End Main Menu -->
                         </div>
                     </div>
                 </div>
             </div>
-            <!--/ End Header Inner -->
-        </header>
+        </div>
+    </div>
+    <!--/ End Header Inner -->
+    </header>
     <!-- header -->
 
     <!-- single page content area start -->
@@ -89,11 +89,12 @@
                     </div>
 
                     <div class="img_thumbnail">
-                    @foreach($products as $prod)
+                        @foreach($products as $prod)
 
                         <div class="single_thumb">
 
-                     <img style="width:200px;height:140px;" class="pic-1 img-fluid" src="{{ asset('product_images/' . $prod->image)}}">
+                            <img style="width:200px;height:140px;" class="pic-1 img-fluid"
+                                src="{{ asset('product_images/' . $prod->image)}}">
 
 
                         </div>
@@ -125,18 +126,18 @@
 
                         <div class="single_page_footer row">
 
-                        <form action="/singlecart" method="POST">
-										@csrf
-										<input type="hidden" name="product_id" value ="{{$product->id}}">
-										<button class="made">Add to cart</button>
-											<input type="hidden" name="quantity" value ="{{$product->quantity}}">
-										</form>
-                                        <form action="/singlefav" method="POST">
-										@csrf
-										<input type="hidden" name="product_id" value ="{{$product->id}}">
-										<button class="made">Add To Wishlist</button>
-											<input type="hidden" name="quantity" value ="{{$product->quantity}}">
-										</form>
+                            <form action="/singlecart" method="POST">
+                                @csrf
+                                <input type="hidden" name="product_id" value="{{$product->id}}">
+                                <button class="made">Add to cart</button>
+                                <input type="hidden" name="quantity" value="{{$product->quantity}}">
+                            </form>
+                            <form action="/singlefav" method="POST">
+                                @csrf
+                                <input type="hidden" name="product_id" value="{{$product->id}}">
+                                <button class="made">Add To Wishlist</button>
+                                <input type="hidden" name="quantity" value="{{$product->quantity}}">
+                            </form>
 
                         </div>
                     </div>
@@ -159,49 +160,32 @@
             <div class="tab-content clearfix  tab_body">
                 <div class="tab-pane active" id="1a">
                     <div class="tab_one_c">
-                              <p>{{$product->description}}</p>
+                        <p>{{$product->description}}</p>
 
                     </div>
                 </div>
                 <div class="tab-pane" id="2a">
                     <div class="review_area">
                         <div class="rev_title">
-                            <h4>Review By Koisk</h4>
+                            <h4>Write your Review</h4>
                         </div>
-                        <ul class="review_list">
-                            <li><span>Quality: </span>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                            </li>
-                            <li><span>price: </span>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star-o"></i>
-                            </li>
-                            <li><span>value: </span>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star-o"></i>
-                            </li>
-                        </ul>
-                        <h6>Write A Review</h6>
-                        <form action="#" class="review_from">
-
-                            <label for="r_name">name</label>
-                            <input type="text" id="r_name" class="c_name name r_name">
-                            <label for="r_sum">summary</label>
-                            <input type="emial" id="r_sum" class="c_email email">
-                            <label for="r_rev">review</label>
-                            <textarea name="c_from" id="r_rev" class="c_m rev r_rev"></textarea>
+                        <form action="/review" class="review_from" method="POST">
+                            @csrf
+                            <label for="r_sum">review</label>
+                            <input name="content" type="text" id="r_sum" class="c_email email" require>
+                            <input name="pro_id" type="hidden" id="pro_id" value="{{$product->id}}">
                             <button class="btnn wr_c_btn c_btn reb_btn">Sumbit</button>
+                            <input name="pro_category" type="hidden" id="pro_cat" value="{{$product->category}}">
                         </form>
+                        @if(isset($reviews)&&!empty($reviews))
+                        @foreach($reviews as $review)
+                        <p>{{$review}}</p>
+                        @endforeach
+                        @else
+                        <p>No reviews yet</p>
+                        @endif
+                        <p>{{session('user.fullname')}}</p>
+                        <p></p>
                     </div>
                 </div>
 
@@ -217,14 +201,16 @@
                 <h2>Related Product</h2>
                 <div class="products_area sing_page_pr_slide">
                     <!-- single-product -->
-                   @foreach($relatedproducts as $produ)
+                    @foreach($relatedproducts as $produ)
 
                     <div class="single_product">
                         <div class="single_img_slide">
                             <div class="sng_im_p">
                                 <a href="{{url('/details' ,[$produ->id , $produ->category])}}">
-                                    <div class="front_img"><img style="width:280px;height:240px;" src="{{ asset('product_images/' . $produ->image)}}" ></div>
-                                    <div class="back_img"><img style="width:280px;height:240px;" src="{{ asset('product_images/' . $produ->image)}}" ></div>
+                                    <div class="front_img"><img style="width:280px;height:240px;"
+                                            src="{{ asset('product_images/' . $produ->image)}}"></div>
+                                    <div class="back_img"><img style="width:280px;height:240px;"
+                                            src="{{ asset('product_images/' . $produ->image)}}"></div>
                                 </a>
                             </div>
                             <div class="si_img_ic prod_content_area">
@@ -238,8 +224,9 @@
                                     </a>
                                     <a href="#" data-toggle="tooltip" title="Add to Cart">
                                         <i class="fa fa-shopping-cart"></i>
-                                    </a> 
-                                    <a href="{{url('/details' ,[$produ->id , $produ->category])}}" data-toggle="tooltip" title="Product details">
+                                    </a>
+                                    <a href="{{url('/details' ,[$produ->id , $produ->category])}}" data-toggle="tooltip"
+                                        title="Product details">
                                         <i class="fa fa-link"></i>
                                     </a>
                                 </div>
@@ -248,32 +235,32 @@
                     </div>
 
                     @endforeach
+                </div>
+            </div>
+    </section>
+    <!-- .single_slide_area end -->
+    <!-- Start Shop Newsletter  -->
+    <section class="shop-newsletter section">
+        <div class="container">
+            <div class="inner-top">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2 col-12">
+                        <!-- Start Newsletter Inner -->
+                        <div class="inner">
+                            <h4>Newsletter</h4>
+                            <p> Subscribe to our newsletter and get <span>10%</span> off your first purchase</p>
+                            <form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
+                                <input name="EMAIL" placeholder="Your email address" required="" type="email">
+                                <button class="btn">Subscribe</button>
+                            </form>
+                        </div>
+                        <!-- End Newsletter Inner -->
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <!-- .single_slide_area end -->
-<!-- Start Shop Newsletter  -->
-<section class="shop-newsletter section">
-		<div class="container">
-			<div class="inner-top">
-				<div class="row">
-					<div class="col-lg-8 offset-lg-2 col-12">
-						<!-- Start Newsletter Inner -->
-						<div class="inner">
-							<h4>Newsletter</h4>
-							<p> Subscribe to our newsletter and get <span>10%</span> off your first purchase</p>
-							<form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
-								<input name="EMAIL" placeholder="Your email address" required="" type="email">
-								<button class="btn">Subscribe</button>
-							</form>
-						</div>
-						<!-- End Newsletter Inner -->
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- End Shop Newsletter -->
+    <!-- End Shop Newsletter -->
 
     @include("user.layouts.footer")
     <!-- js section -->
--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\user\UserController;
 use App\Http\Controllers\user\ProductController;
+use App\Http\Controllers\user;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,24 +22,28 @@ use App\Http\Controllers\user\ProductController;
 // Route::get('/home', function () {
 //     return redirect('/');
 // });
-Route::get('/home',[ProductController ::class,'showhomeproduct']);
-Route::get('/',[ProductController ::class,'showhomeproduct']);
+Route::get('/home', [ProductController::class, 'showhomeproduct']);
+Route::get('/', [ProductController::class, 'showhomeproduct']);
 /********* our categories (show)****** */
 
 
-Route::get('/allcategories',[ProductController ::class,'showhome']);
+Route::get('/allcategories', [ProductController::class, 'showhome']);
 
-Route::get('/electronices',[ProductController ::class,'showelectronies']);
-Route::get('/books',[ProductController ::class,'showbooks']);
-Route::get('/cosmatics',[ProductController ::class,'showcosmatics']);
-Route::get('/furniture',[ProductController ::class,'showfurniture']);
-Route:: get('/fashion',[ProductController::class,'show']);
+Route::get('/electronices', [ProductController::class, 'showelectronies']);
+Route::get('/books', [ProductController::class, 'showbooks']);
+Route::get('/cosmatics', [ProductController::class, 'showcosmatics']);
+Route::get('/furniture', [ProductController::class, 'showfurniture']);
+Route::get('/fashion', [ProductController::class, 'show']);
 
 
 //checkout
-Route::get('/checkout', function () {return view('user.products.checkout ');});
+Route::get('/checkout', function () {
+    return view('user.products.checkout ');
+});
 //contactus
-Route::get('/contactus', function () {return view('user.products.contactus  ');});
+Route::get('/contactus', function () {
+    return view('user.products.contactus  ');
+});
 Route::get('/signin', function () {
     return view('user.Auth.signin');
 });
@@ -52,52 +57,56 @@ Route::get('/forgetpass', function () {
 //cart details
 // Route::get('/cart', function () {return view('user.products.cartdetails');});
 //profile
-Route::get('/profile', function () {return view('user.products.profile');});
+Route::get('/profile', function () {
+    return view('user.products.profile');
+});
 
-Route::get('/cartdetails/{id}/{quantity}',[ProductController::class,'updateCartProduct']);
+Route::get('/cartdetails/{id}/{quantity}', [ProductController::class, 'updateCartProduct']);
 
 
 //add to cart
-Route::post("fashioncart",[ProductController::class,'addtocart']);
-Route::post("furniturecart",[ProductController::class,'addtocart']);
-Route::post("electronicescart",[ProductController::class,'addtocart']);
-Route::post("cosmaticscart",[ProductController::class,'addtocart']);
-Route::post("bookscart",[ProductController::class,'addtocart']);
-Route::post("/singlecart",[ProductController::class,'addtocart']);
-Route::get("cartdetails",[ProductController::class,'cartlist']);
+Route::post("fashioncart", [ProductController::class, 'addtocart']);
+Route::post("furniturecart", [ProductController::class, 'addtocart']);
+Route::post("electronicescart", [ProductController::class, 'addtocart']);
+Route::post("cosmaticscart", [ProductController::class, 'addtocart']);
+Route::post("bookscart", [ProductController::class, 'addtocart']);
+Route::post("/singlecart", [ProductController::class, 'addtocart']);
+Route::get("cartdetails", [ProductController::class, 'cartlist']);
 
-Route::get("removecart/{id}",[ProductController::class,'removecart']);
+Route::get("removecart/{id}", [ProductController::class, 'removecart']);
 
 
 //wishlist
 
-Route::post("fashionfav",[ProductController::class,'addtofav']);
-Route::post("furniturefav",[ProductController::class,'addtofav']);
-Route::post("electronicesfav",[ProductController::class,'addtofav']);
-Route::post("cosmaticsfav",[ProductController::class,'addtofav']);
-Route::post("booksfav",[ProductController::class,'addtofav']);
-Route::post("/singlefav",[ProductController::class,'addtofav']);
-Route::get("favdetails",[ProductController::class,'favlist']);
-Route::get("removefav/{id}",[ProductController::class,'removefav']);
-Route::get("removeall/{id}",[ProductController::class,'removeall']);
+Route::post("fashionfav", [ProductController::class, 'addtofav']);
+Route::post("furniturefav", [ProductController::class, 'addtofav']);
+Route::post("electronicesfav", [ProductController::class, 'addtofav']);
+Route::post("cosmaticsfav", [ProductController::class, 'addtofav']);
+Route::post("booksfav", [ProductController::class, 'addtofav']);
+Route::post("/singlefav", [ProductController::class, 'addtofav']);
+Route::get("favdetails", [ProductController::class, 'favlist']);
+Route::get("removefav/{id}", [ProductController::class, 'removefav']);
+Route::get("removeall/{id}", [ProductController::class, 'removeall']);
 /**************search and product details routes************ */
 //search
-Route::get('/search',[ProductController::class , "searchProduct"]);
+Route::get('/search', [ProductController::class, "searchProduct"]);
 
 //details for each product
- Route::get('/details/{id}/{category}',[ProductController::class , "detailsProduct"]);
+Route::get('/details/{id}/{category}', [ProductController::class, "detailsProduct"]);
+
+Route::post('/review', [ProductController::class, "store"]);
 
 /*********** registration routes************** */
 //user signIn
-Route::get('/login',[UserController::class , "loginForm"]);
-Route::post('/login',[UserController::class , "handleLogin"]);
+Route::get('/login', [UserController::class, "loginForm"]);
+Route::post('/login', [UserController::class, "handleLogin"]);
 
 //user signUp
-Route::get('/register',[UserController::class , "registerForm"]);
-Route::post('/register',[UserController::class , "handleForm"]);
+Route::get('/register', [UserController::class, "registerForm"]);
+Route::post('/register', [UserController::class, "handleForm"]);
 
 // user logout
-Route::get('/logout' , function(){
+Route::get('/logout', function () {
     Session::forget('user');
     return view('user.Auth.signin');
 });
@@ -106,6 +115,5 @@ Route::get('/logout' , function(){
 
 
 // checkout
-Route::post('success', [ UserController::class, "userUpdate" ]);
-Route::post("checkout",[ProductController::class,'showCheckout']);
-
+Route::post('success', [UserController::class, "userUpdate"]);
+Route::post("checkout", [ProductController::class, 'showCheckout']);
