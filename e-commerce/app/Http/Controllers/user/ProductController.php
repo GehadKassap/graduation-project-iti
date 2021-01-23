5d17c075dadfc0232c6e5b750002c279baa16349
kholud shokry <eng.kholud.shokry@gmail.com>
2021-01-22 17:41:29 -0800

    edit in add to favorite function

--- e-commerce/app/Http/Controllers/user/ProductController.php
+++ e-commerce/app/Http/Controllers/user/ProductController.php
@@ -290,10 +290,9 @@ class ProductController extends Controller
         return view('user.products.favorite', ['products' => $products, 'fav' => $fav, 'suggest' =>$suggest]);
     }
 
-
-    function removefav($rowid)
+    function removefav($id)
     {
-        Fav::destroy($rowid);
+        $del = Fav::destroy($id);
         return redirect('favdetails');
     }
     public function removeall()
